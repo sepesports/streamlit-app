a77b0204e78b92a148a94940da824e18967f8941
sepesports <marketinclicknova@gmail.com>
2026-02-10 14:23:44 -0500

    Update admin.py

--- pages/admin.py
+++ pages/admin.py
@@ -3,11 +3,14 @@ import streamlit as st
 if not st.session_state.get("auth"):
     st.switch_page("app.py")
 
+st.set_page_config(layout="wide")
 st.title("Panel Admin")
 
-st.write(f"Usuario conectado: {st.session_state.get('user')}")
+st.write(f"Usuario: {st.session_state.get('user','')}")
+st.write(f"Rol: {st.session_state.get('role','')}")
 
 if st.button("Cerrar sesión"):
     st.session_state.auth = False
     st.session_state.user = ""
+    st.session_state.role = ""
     st.switch_page("app.py")
