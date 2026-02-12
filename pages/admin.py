8db136e4d7e23f8d8de4863d0924ef89fd7f36d8
sepesports <marketinclicknova@gmail.com>
2026-02-12 16:25:04 -0500

    Update admin.py

--- pages/admin.py
+++ pages/admin.py
@@ -244,8 +244,8 @@ html, body{
 /* INPUTS */
 input.field{
   position:absolute;
-  left:25%; /* 16 */
-  right:25%; /* 16 */
+  left:24%; /* 16 */
+  right:24%; /* 16 */
   height:10%; /* 16 */
   border: 1px solid rgba(255,255,255,.55);
   border-radius: 999px;
