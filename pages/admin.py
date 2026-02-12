21aa2243107c6ddf1ccd7a8f0f482cb5b01ebe79
sepesports <marketinclicknova@gmail.com>
2026-02-12 15:53:22 -0500

    Update admin.py

--- pages/admin.py
+++ pages/admin.py
@@ -81,8 +81,8 @@ html = """
      CONTROLES MÓVIL
      Cambia AQUÍ posición/tamaño para móvil
      ========================================================= */
-  --logoWMobile: 140px;      /* tamaño logo móvil */
-  --logoTopMobile: 5%;       /* subir/bajar logo móvil */
+  --logoWMobile: 150px;      /* tamaño logo móvil */
+  --logoTopMobile: 7%;       /* subir/bajar logo móvil */
   --logoXMobile: 0px;        /* mover logo izq/der móvil (px) */ Estoy aqui
 
   --titleTopMobile: 10%;     /* subir/bajar título móvil */
